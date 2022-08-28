46116bce610547cf62c963066cc0bc8bc61a5942
PrimYA <biawispolish@gmail.com>
2022-08-28 19:10:39 -0300

    import done

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD1L4TDO8Af9hOCHDfW_qf4CtE0XVAMlqk",
+    authDomain: "mundo-invertido-week.firebaseapp.com",
+    projectId: "mundo-invertido-week",
+    storageBucket: "mundo-invertido-week.appspot.com",
+    messagingSenderId: "564675325125",
+    appId: "1:564675325125:web:6a0553487d164e707d14ac"
+  }
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig)
+  export default app
